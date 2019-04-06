2077144036e5d1ce5903285a222376ed2dd3bfee
stigi99 <misiakmateusz@yahoo.com>
2019-04-06 14:06:27 +0200

    FORMULEK

---
+++ zadania/obliczanie_pola.php
@@ -0,0 +1,46 @@
+<?php
+//POLE KWADRATU
+if (!isset($_POST["potwierdz"]) || !isset($_GET["potwierdz"]))
+    header("Location: pole_kwadratu.html");
+
+if (isset($_POST["bok2"])){
+$bok2=$_POST["bok2"];
+if( !is_numeric($bok2) || $bok2<0 ){
+    
+
+    echo("Wprowadziles zle dane popraw je bo jedynka ");
+    
+
+    header('Refresh: 10; url=pole_kwadratu.html');
+}
+else {
+ //$bok=$_POST["bok"];
+ //$pole=$bok*$bok;
+//$pole=$bok*$bok;
+$pole2=$bok2*$bok2;
+echo ("POLE WYNOSI $pole2");
+}
+}
+
+
+//POLE KOLA
+
+
+if (isset($_GET["promien"]))
+{
+$promien=$_GET["promien"];
+if ($promien<0 || !is_numeric($promien) ){
+    echo("Wprowadziles zle dane popraw je");
+    header('Refresh: 10; url=pole_kwadratu.html');
+}
+else {
+$pole3=M_PI * $promien * $promien;
+echo ("POLE WYNOSI $pole3");
+}
+}
+
+
+
+
+
+?>
