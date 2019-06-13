b838029d99f3ccdccca3187d3e87a8ca4f0dc59d
stigi99 <misiakmateusz@yahoo.com>
2019-06-14 00:57:29 +0200

    losowanie i naprawa błedu

--- memory/memory.js
+++ memory/memory.js
@@ -5,35 +5,38 @@ var cards = ["1.jpg", "2.jpg", "3.jpg", "3.jpg", "4.jpg", "5.png", "2.jpg", "6.j
 
 //console.log(cards);
 
-var c0 = document.getElementById('c0');
-var c1 = document.getElementById('c1');
-var c2 = document.getElementById('c2');
-var c3 = document.getElementById('c3');
+var $karta = $(".card");
+//console.log($karta[2]);
+$(".card").detach();
+function losowanie(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+		a[j] = x;
+$(".board").append(a);
+    }
+    return a;
+}
 
-var c4 = document.getElementById('c4');
-var c5 = document.getElementById('c5');
-var c6 = document.getElementById('c6');
-var c7 = document.getElementById('c7');
+losowanie($karta)
 
-var c8 = document.getElementById('c8');
-var c9 = document.getElementById('c9');
-var c10 = document.getElementById('c10');
-var c11 = document.getElementById('c11');
+var c0 = $('#c0').click(function() { revealCard(0); });
+var c1 = $('#c1').click(function() { revealCard(1); });
+var c2 = $('#c2').click(function() { revealCard(2); });
+var c3 = $('#c3').click(function() { revealCard(3); });
 
-c0.addEventListener("click", function() { revealCard(0); });
-c1.addEventListener("click", function() { revealCard(1); });
-c2.addEventListener("click", function() { revealCard(2); });
-c3.addEventListener("click", function() { revealCard(3); });
+var c4 = $('#c4').click(function() { revealCard(4); });
+var c5 = $('#c5').click(function() { revealCard(5); });
+var c6 = $('#c6').click(function() { revealCard(6); });
+var c7 = $('#c7').click(function() { revealCard(7); });
 
-c4.addEventListener("click", function() { revealCard(4); });
-c5.addEventListener("click", function() { revealCard(5); });
-c6.addEventListener("click", function() { revealCard(6); });
-c7.addEventListener("click", function() { revealCard(7); });
+var c8 = $('#c8').click(function() { revealCard(8); });
+var c9 = $('#c9').click(function() { revealCard(9); });
+var c10 = $('#c10').click(function() { revealCard(10); });
+var c11 = $('#c11').click(function() { revealCard(11); });
 
-c8.addEventListener("click", function() { revealCard(8); });
-c9.addEventListener("click", function() { revealCard(9); });
-c10.addEventListener("click", function() { revealCard(10); });
-c11.addEventListener("click", function() { revealCard(11); });
 
 var oneVisible = false;
 var turnCounter = 0;
@@ -47,7 +50,7 @@ function revealCard(nr)
 	
 	//alert('Opacity: '+opacityValue);
 	
-	if (opacityValue != 0 && lock == false)
+	if (opacityValue != 0 && lock == false && nr != visible_nr )
 	{
 		lock = true;
 		
@@ -86,7 +89,7 @@ function revealCard(nr)
 			}
 			
 			turnCounter++;
-			$('.score').html('Turn counter: '+turnCounter);
+			$('.score').html('Licznik tur: '+turnCounter);
 			oneVisible = false;
 		}
 		
